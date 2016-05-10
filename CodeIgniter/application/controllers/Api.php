79edbe729cc073a124a8f034007fd84e34ab4b19
Sean Rock <sean@sparxitsolutions.com>
2016-05-10 18:13:31 +0530

    update

--- CodeIgniter/application/controllers/Api.php
+++ CodeIgniter/application/controllers/Api.php
@@ -91,6 +91,7 @@ class Api extends CI_Controller {
         if ($is_authorized) {
             // this is only here so that you get to see your code in the cURL request. Otherwise, we'd redirect back to the client
             $code = substr($response->getHttpHeader('Location'), strpos($response->getHttpHeader('Location'), 'code=') + 5, 40);
+            //print_r($response);
             exit($code);
         }
 
